5495b06f55833580422698a45889b2394482567d
HoneyYolk <Honey_Yolk@outlook.com>
2021-11-28 20:46:47 +0800

    message test

--- client/src/main.rs
+++ client/src/main.rs
@@ -5,13 +5,13 @@ use uuid::Uuid;
 
 fn main() -> std::io::Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:8888")?;
-    let user_id = Uuid::new_v4();
+    let client_id = Uuid::new_v4();
     loop {
         let mut text = String::new();
         io::stdin()
             .read_line(&mut text)
             .expect("Failed to read line");
-        let msg = Message::new(user_id, text);
+        let msg = Message::new(client_id, text);
         stream.write_all(serde_json::to_string(&msg).unwrap().as_bytes())?;
     }
 }
--- lib/src/message.rs
+++ lib/src/message.rs
@@ -2,13 +2,13 @@ use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 #[derive(Deserialize, Serialize, Debug)]
-pub struct Message {
-    user_id: Uuid,
-    msg: String,
+pub struct Message<T> {
+    client_id: Uuid,
+    msg: T,
 }
 
-impl Message {
-    pub fn new(user_id: Uuid, msg: String) -> Self {
-        Self { user_id, msg }
+impl<T> Message<T> {
+    pub fn new(client_id: Uuid, msg: T) -> Self {
+        Self { client_id, msg }
     }
 }
